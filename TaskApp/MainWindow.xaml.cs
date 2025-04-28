0a9d21d83920ef9b1f889784ef9e589602d7b28c
Brian <mythic.is.online@gmail.com>
2025-04-28 01:51:29 -0400

    Variety of changes; mainly work on UX and UI of the task menu.

--- TaskApp/MainWindow.xaml.cs
+++ TaskApp/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Channels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,6 +29,8 @@ namespace TaskApp
             solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName!;
             solutionDataPath = Path.Combine(solutionDirectory, "Data");
 
+            this.Topmost = true;
+
             // Extra
             var dataFiles = Directory.GetDirectories(solutionDataPath);
 
@@ -40,14 +44,24 @@ namespace TaskApp
              }
         }
         
-        public void OpenTaskWindow(string taskName)
+        static void OpenTaskWindow(string taskPath)
         {
+            // New Window
+            TaskWindow newWindow = new TaskWindow();
+            newWindow.Title = Path.GetFileName(taskPath);
+            newWindow.Topmost = true;
+            newWindow.Show();
 
+            // Extra
+            Application.Current.MainWindow.Close();
         }
 
         private void OpenTaskLog_Click(object sender, RoutedEventArgs e)
         {
-            
+            int selectedIndex = TaskComboBox.SelectedIndex;
+            var dataFiles = Directory.GetDirectories(solutionDataPath);
+
+            OpenTaskWindow(dataFiles[selectedIndex]);
         }
         
         private void DeleteTaskLog_Click(object sender, RoutedEventArgs e)
@@ -55,7 +69,11 @@ namespace TaskApp
             int selectedIndex = TaskComboBox.SelectedIndex;
             var dataFiles = Directory.GetDirectories(solutionDataPath);
 
-            Debug.WriteLine(dataFiles[selectedIndex]);
+            if (selectedIndex != -1)
+            {
+                TaskComboBox.Items.RemoveAt(selectedIndex);
+                Directory.Delete(dataFiles[selectedIndex], true);
+            }
         }
 
         private void CreateTaskLog_Click(object sender, RoutedEventArgs e)
@@ -69,6 +87,8 @@ namespace TaskApp
                 if (!Directory.Exists(newLogPath))
                 {
                     Directory.CreateDirectory(newLogPath);
+                    //TaskComboBox.Items.Add(Path.GetFileName(newLogPath));
+                    OpenTaskWindow(newLogPath);
                 }
             };
         }
---
+++ TaskApp/TaskWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskApp
+{
+    public partial class TaskWindow : Window
+    {
+        public enum ImportanceType { Crucial, Moderate, Low };
+
+        public class Task
+        {
+            public string? TaskInfo { get; set; }
+            public ImportanceType Importance { get; set; }
+        }
+
+        public TaskWindow()
+        {
+            InitializeComponent();
+
+            List<Task> taskItems = new List<Task>();
+            taskItems.Add(new Task() { TaskInfo = "John Doe", Importance = ImportanceType.Crucial });
+            taskItems.Add(new Task() { TaskInfo = "Jane Doe", Importance = ImportanceType.Moderate });
+            taskItems.Add(new Task() { TaskInfo = "Sammy Doe", Importance = ImportanceType.Low });
+            listViewTasks.ItemsSource = taskItems;
+
+            CollectionView view = (CollectionView) CollectionViewSource.GetDefaultView(listViewTasks.ItemsSource);
+            PropertyGroupDescription groupDescription = new PropertyGroupDescription("Importance");
+            view.GroupDescriptions.Add(groupDescription);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow newWindow = new MainWindow();
+            Application.Current.MainWindow = newWindow;
+            newWindow.Show();
+        }
+    }
+}
